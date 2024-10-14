2ee8433c72b29e2127caa4e9554fcc1d1c2d4192
StasPlov <SaviouR.S@mail.ru>
2024-10-14 05:04:11 +0000

    fix: ShipmentSetRestrictionRequest

--- src/JdeShipping.php
+++ src/JdeShipping.php
@@ -25,6 +25,7 @@ use JdeShipping\Request\Request;
  * 
  * @method Dto\ShipmentStatus[]			getShipmentNewStatus(Request\Shipment\ShipmentNewStatusRequest $request);
  * @method Dto\ShipmentSimpleStatus 	getShipmentStatusByCode(Request\Shipment\ShipmentStatusByCodeRequest $request);
+ * @method Dto\ShipmentRestriction 	sendShipmentSetRestriction(Request\Shipment\ShipmentSetRestrictionRequest $request);
  */
 final class JdeShipping extends Client
 {
